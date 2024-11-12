f5a6f574c73177571cce02433c0c6ad16a0777a7
Lucca Augusto <lucca.aug@gmail.com>
2024-11-12 19:58:33 -0300

    WIP: Lógica inicial do problema da Inversão

--- TP1/1550/inversao.py
+++ TP1/1550/inversao.py
@@ -12,6 +12,35 @@ Saída
 Para cada caso de teste, o programa deve imprimir um inteiro indicando o número mínimo de apertos de botão para que o número do visor passe de A para B.
 """
 
+class Aparelho:
+    A: int
+    B: int
+    numero_atual: int
+    numero_de_botoes_apertados: int
+
+    def __init__(self, a: int, b: int) -> None:
+        self.A = a
+        self.B = b
+
+        self.numero_de_botoes_apertados = 0
+        self.numero_atual = a
+
+    def _incrementa(self) -> None:
+        self.numero_de_botoes_apertados += 1
+        self.numero_atual += 1
+
+    def _inverte(self) -> None:
+        self.numero_de_botoes_apertados += 1
+        numero_str = str(self.numero_atual)
+        numero_invertido_str = numero_str[::-1]
+        self.numero_atual = int(numero_invertido_str)
+
+    def __str__(self) -> str:
+        _s = f"A: {self.A}\n"
+        _s += f"B: {self.B}\n"
+        _s += f"numero_de_botoes_apertados: {self.numero_de_botoes_apertados}\n"
+        _s += f"numero_atual: {self.numero_atual}\n"
+        return _s
 
 if __name__ == '__main__':
     T = int(input())
@@ -19,3 +48,6 @@ if __name__ == '__main__':
         linha_com_a_b = input().split()
         A = int(linha_com_a_b[0])
         B = int(linha_com_a_b[1])
+
+        aparelho = Aparelho(A, B)
+        print(aparelho)
