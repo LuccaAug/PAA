6ea0be7724e093e6ec9c7cfb530a1f99f8a1d6bf
Lucca Augusto <lucca.aug@gmail.com>
2024-12-17 11:34:53 -0300

    Estrutura inicial de todos os problemas restantes do TP2

---
+++ TP2/1027/onda-critica.py
@@ -0,0 +1,19 @@
+"""
+A tarefa é simples. Através de alguns pontos críticos em 2D, você deve desenhar uma onda como uma curva. Seu objetivo é incluir tantos pontos quantos forem possível.
+
+https://resources.beecrowd.com/gallery/images/problems/UOJ_1027.png
+
+Haverá uma linha imaginária y = a, a qual chamaremos de eixo principal para a curva.
+Todos os pontos na curva deverão ter diferentes coordenadas x. Suas coordenadas y devem ser na forma a-1 ou a+1.
+Dois pontos consecutivos na curva devem ter diferença de 2 na coordenada y.
+
+Entrada
+Haverá no máximo, 222 casos de testes. Cada caso inicia com um inteiro N, que é o número de pontos no caso de teste. Nas próximas N linhas, haverá N pares de inteiros dando as coordenadas x e y de cada ponto. Não haverá mais do que 1000 pontos em cada caso de teste. Todas coordenadas são inteiros – eles devem ficar dentro de um inteiro de 2 bytes com sinal. Os dados devem ser lidos da entrada padrão.
+
+Saída
+Para cada caso de teste, imprima um número – o número máximo de pontos críticos que podem ser incluídos em uma curva desenhada a partir dos pontos dados.
+"""
+
+if __name__ == '__main__':
+    # !TODO: fazer o programa, apenas estrutura montada
+    pass
